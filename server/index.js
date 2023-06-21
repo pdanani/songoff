1ff6d8e6803d6d6edb1d908c497b493c82f14a67
Pawan <58373479+pdanani@users.noreply.github.com>
2023-06-20 23:41:41 -0400

    cron scheduler to reset songs everyday

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { schema } from './schema/schema';
 import { resolvers } from './resolvers/resolvers';
-
+import cron from 'node-cron';
+import { fetchSongsFromSpotify } from './utils/spotify';
 
 const app = express();
 
@@ -12,6 +13,20 @@ const server = new ApolloServer({
 });
 
 server.applyMiddleware({ app });
+
+// Schedule the task to run every day at 12:00 AM PST
+cron.schedule('0 0 * * *', () => {
+  // Fetch new songs from the Spotify API
+  fetchSongsFromSpotify()
+    .then((songs) => {
+      console.log('Fetched new songs:', songs);
+      // Update the songs data in your database or wherever you store it
+      // ...
+    })
+    .catch((error) => {
+      console.error('Error fetching songs:', error);
+    });
+});
 
 app.listen({ port: 4000 }, () =>
   console.log(`Server running at http://localhost:4000${server.graphqlPath}`)
