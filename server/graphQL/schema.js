b9032b5a278d7dbddeae12eed36d7095a534e748
Pawan <58373479+pdanani@users.noreply.github.com>
2023-07-07 19:00:15 -0400

    Created schema (song and vote type)

---
+++ server/graphQL/schema.js
@@ -0,0 +1,31 @@
+import { gql } from 'apollo-server-express';
+
+const typeDefs = gql`
+  type Song {
+    videoId: String!
+    title: String!
+    artist: String!
+  }
+  type Song{
+    videoID: String!
+    title: String!
+    artist: String
+  }
+
+  type Vote {
+    id: ID!
+    userId: Int!
+    songId: String!
+    song: Song!
+  }
+
+  type Query {
+    songs: [Song!]!
+  }
+
+  type Mutation {
+    voteSong(userId: Int!, songId: String!): Vote!
+  }
+`;
+
+export default typeDefs;
