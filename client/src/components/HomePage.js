701eee72ecda136c202e92c9b54f9ef1f8a811d0
Pawan <58373479+pdanani@users.noreply.github.com>
2023-07-12 19:51:38 -0400

    Fix: Was calling get_songs incorrectly

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,14 +1,32 @@
-import SongList from "./SongList"
-import SongCard from "./SongCard"
+import React, { useEffect, useState } from 'react';
+import { useQuery } from '@apollo/client';
+import { GET_SONGS } from '../graphql/queries';
+import SongList from '../components/SongList';
 
-const HomePage = () =>{
+const HomePage = () => {
+    const { loading, error, data } = useQuery(GET_SONGS);
+    const [songs, setSongs] = useState([]);
 
+    useEffect(() => {
+        if (data && data.songs) {
+            setSongs(data.songs);
+        }
+    }, [data]);
 
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
 
     return (
+        <div>
+            <h1>Today's Songs</h1>
+            <SongList songs={songs} />
+        </div>
+    );
+};
 
-    <div>homepage</div>
-    )
-}
-
-export default HomePage
+export default HomePage;
--- client/src/components/SongList.js
+++ client/src/components/SongList.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import { SongListContainer } from '../styles/SongListStyles';
+import { GET_SONGS } from '../graphql/queries';
 
 const SongList = () => {
+    const { loading, error, data } = useQuery(GET_SONGS);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
+
+    const songs = data.songs;
 
     return (
         <SongListContainer>
             <h2>Song List</h2>
+            {songs.map((song) => (
+                <div key={song.id}>
+                    <h3>{song.title}</h3>
+                    <p>Artist: {song.artist}</p>
+                </div>
+            ))}
         </SongListContainer>
     );
 }
